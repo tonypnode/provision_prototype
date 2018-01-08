d461dd6599d9aee5e7f6b9c91e8eb37eb098d413
tonypnode <sgtp@me.com>
2018-01-08 14:57:48 -0500

    Begin test development, add some TODOs
    Signed-off-by: tonypnode <sgtp@me.com>

--- auto_config/forms.py
+++ auto_config/forms.py
@@ -6,11 +6,15 @@ class NewProvision(forms.Form):
     # TODO: Need to find a different way (prolly db) to get the sites,
     #       because each time a new file is added, the web needs to be restarted... not good
     sites = [[x, x.strip(' site data . json')] for x in ls('data_files/sites') if 'site_' in x]
+
+    # TODO: Should this be in a DB somewhere?
     dev_functs = [
         ['AS', 'Access Switch', ],
         ['DS', 'Distro Switch'],
         ['R', 'Router']
         ]
+    # TODO: The device function choice should also have the ability to modify the template
+    #       that is chosen. Switches don't need routing config
 
     site = forms.ChoiceField(choices=sites)
     device_function = forms.ChoiceField(choices=dev_functs)
--- auto_config/tests.py
+++ auto_config/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HTMLPageTest(TestCase):
+    """
+    Validates templates and redirects
+
+    """
+    def check_html(self, url, correct_template, correct_status_code):
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, correct_template)
+        self.assertEqual(str(response.status_code), correct_status_code)
+
+    def test_home_page_returns_correct_template(self):
+        """
+        Test proper template for home page
+
+        """
+        self.check_html('/', 'home.html', '200')
